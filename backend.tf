fcac6ccd69a46ee67ab42f1861d0d7b727d4409c
alopez24 <lopez.agus244@gmail.com>
2025-05-30 15:14:47 -0300

    cambio de s3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket                 = "mundosse22"
+    bucket                 = "pin2"
     region                 = "us-east-2"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
